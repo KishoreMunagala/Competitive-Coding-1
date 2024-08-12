8b0cd89995a06ea90322aa6dc8422cf0f41d94b8
KishoreMunagala <kishorereddy72707@gmail.com>
2024-08-12 16:23:47 -0700

    competitive 1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,31 @@
+Time complexity:O(logn)
+Space complexity:o(1)
 
+class Solution {
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, int arr[]) {
+
+        int low = 0;
+        int high = arr.length-1;
+        int mid = 0;
+        
+        while(high-low>=2)
+        {
+           mid = low + (high-low)/2;
+           int middiff = arr[mid]-mid;
+           int leftdiff = arr[low]-low;
+           int rightdiff = arr[high]-high;
+           
+           if(middiff != leftdiff)
+           {
+               high = mid;
+           }
+           else if(middiff != rightdiff)
+           {
+               low = mid;
+           }
+        }
+        return (arr[high]+arr[low])/2;
+    }
+}
